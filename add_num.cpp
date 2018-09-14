86469498084988d0540635a01342383b882298cb
vishwesh5 <vishweshshrimali5@gmail.com>
2018-09-14 20:05:29 +0530

    Create add_num.cpp

---
+++ add_num.cpp
@@ -0,0 +1,18 @@
+/*
+ * problem link: https://www.codechef.com/problems/FLOW001
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        int t, a, b;
+        cin >> t;
+        for (int i=0; i < t; i++)
+        {
+                cin >> a >> b;
+                cout << a+b << "\n";
+        }
+}
