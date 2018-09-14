7944fa0b8e4c5ce62f380a6580168806769f1785
vishwesh5 <vishweshshrimali5@gmail.com>
2018-09-14 09:12:57 +0530

    Create A_plus_B.cpp

---
+++ A_plus_B.cpp
@@ -0,0 +1,29 @@
+/*
+University of Helsinki training
+Fall 2018, on Thursdays in Kumpula
+*/
+
+/*
+Problem statement: https://cses.fi/210/task/A
+Given two numbers A and B, calculate their sum A+B.
+
+Input
+
+The first line of input consists of two space separated numbers, A and B. Note that the numbers might be negative.
+
+Output
+
+Output a single integer A+B. 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        int A,B,sum;
+        cin >> A >> B;
+        sum = A+B;
+        cout << sum;
+}
