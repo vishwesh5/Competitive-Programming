0374c2fea414424f8aeb45e1e1bca93349f6f1da
vishwesh5 <vishweshshrimali5@gmail.com>
2018-09-15 09:30:04 +0530

    Create numberMirror.cpp

---
+++ numberMirror.cpp
@@ -0,0 +1,18 @@
+/*
+ * Problem link: https://www.codechef.com/problems/START01
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        unsigned int n;
+
+        cin >> n;
+
+        cout << n << "\n";
+
+        return 0;
+}
