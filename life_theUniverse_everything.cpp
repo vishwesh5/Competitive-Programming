d1f8b4ebd77a9a43f8650a0f020566bb45c44721
vishwesh5 <vishweshshrimali5@gmail.com>
2018-09-17 07:59:37 +0530

    Add files via upload

---
+++ life_theUniverse_everything.cpp
@@ -0,0 +1,20 @@
+/*
+ * problem link: https://www.codechef.com/problems/TEST
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	while (1)
+	{
+		cin >> N;
+		if (N == 42)
+			break;
+		else
+			cout << N << "\n";
+	}
+}
